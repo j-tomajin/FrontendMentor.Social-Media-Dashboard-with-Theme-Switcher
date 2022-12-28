93655ad0855d9fe4a8805b1b73d0278d3cd41f32
Jerickson Tomajin <tomajinj@gmail.com>
2022-12-28 19:14:29 +0800

    added comments

--- js/app.js
+++ js/app.js
@@ -4,18 +4,23 @@ const slider = document.querySelector('.slider')
 
 // enable dark mode
 const enableDarkMode = () => {
+    // add class darkmode to body
     document.body.classList.add('darkmode')
 
+    // set enabled on localStorage
     localStorage.setItem('darkMode', 'enabled')
 }
 
 // disable dark mode
 const disableDarkMode = () => {
+    // add class darkmode to body
     document.body.classList.remove('darkmode')
 
+    // set disabled on localStorage
     localStorage.setItem('darkMode', null)
 }
 
+// set current theme base on what is on the localStorage
 if(darkMode === 'enabled') {
     enableDarkMode()
     slider.classList.add('slider-toggle')
