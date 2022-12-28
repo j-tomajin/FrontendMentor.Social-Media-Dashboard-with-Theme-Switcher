0e859cc40ba35f45d19551a59e076e82ebb94235
Jerickson Tomajin <tomajinj@gmail.com>
2022-12-28 18:54:54 +0800

    add darkmode

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
 let darkMode = localStorage.getItem('darkMode')
-const toggle = document.querySelector('.switch')
+const darkModeToggle = document.querySelector('#dark-mode')
+const slider = document.querySelector('.slider')
 
-toggle.addEventListener('click', () => {
-    console.log('test')
+// enable dark mode
+const enableDarkMode = () => {
+    document.body.classList.add('darkmode')
+
+    localStorage.setItem('darkMode', 'enabled')
+}
+
+// disable dark mode
+const disableDarkMode = () => {
+    document.body.classList.remove('darkmode')
+
+    localStorage.setItem('darkMode', null)
+}
+
+if(darkMode === 'enabled') {
+    enableDarkMode()
+    slider.classList.add('slider-toggle')
+} else {
+    disableDarkMode()
+    slider.classList.remove('slider-toggle')
+}
+
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode')
+
+    if(darkMode !== 'enabled') {
+        enableDarkMode()
+        slider.classList.add('slider-toggle')
+    } else {
+        disableDarkMode()
+        slider.classList.remove('slider-toggle')
+    }
 })
